fac7c597f944ef2ca8e4838a932e4b79569e1507
immanuel4partner <immanuel4partner@gmail.com>
2025-07-24 19:03:04 +0100

    Scripture Programs

--- week03/ScriptureMemorizer/Program.cs
+++ week03/ScriptureMemorizer/Program.cs
@@ -1,40 +1,46 @@
 
-using System;
-using System.Collections.Generic;
-class program
-{
-    static void Main(string[] args)
-    {
-        // Create a scripture with a reference and text
-        Reference reference = new Reference("Proverbs", 3, 5, 6);
-        Scripture scripture = new Scripture(reference, "Trust in the Lord with all your heart and lean not on your own understanding; in all your ways submit to him, and he will make your paths stright.");
+// I showed creativity by adding a counter to track the amount of words the user has cleared.
 
-        while (!scripture.IsCompletelyHidden())
+using System.Data;
+
+class Program
+{
+    static void Main()
+    {
+        Reference reference = new Reference("Proverbs", 3, 5, 6);
+        string[] text = "Trust in the Lord with all thine heart and lean not unto thine own understanding. In all thy ways acknowledge Him, and He shall direct thy paths.".Split(' ');
+        int words = text.Count();
+
+
+        Scripture scripture = new Scripture(reference, string.Join(" ", text));
+
+        //Initialize the counter to zero;
+        int count = 0;
+        // Set amount of random words to clear at a time.
+        int numToHide = 3;
+
+        
+
+        while (true)
         {
             Console.Clear();
             Console.WriteLine(scripture.GetDisplayText());
-            Console.WriteLine("\nPress Enter to hide words or type 'quit' to exit.");
+            Console.WriteLine("\nPress Enter to hide more words or type 'quit' to exit.");
+            Console.WriteLine($"You have cleared {count} of {words} words.");
 
             string input = Console.ReadLine();
-            if (input.ToLower() == "quit")
+            if (input.ToLower() == "quit") break;
+
+            if (scripture.IsCompletelyHidden())
+            {
+                Console.WriteLine($"All words are hidden. Press Enter to exit.");
+                Console.ReadLine();
                 break;
+            }
 
-            scripture.HideRandomWords(2);
+            scripture.HideRandomWords(numToHide); // or any number you want
+            count += numToHide;
+
         }
-
-        Console.Clear();
-        Console.WriteLine(scripture.GetDisplayText());
-        Console.WriteLine("\nAll words are hidden. Program ended.");
     }
-
 }
-
-
-
-
-
-
-
-
-
-
