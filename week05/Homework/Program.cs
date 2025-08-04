0b828d59e4c3522633b17f8f00001e7f825c383d
immanuel4partner <immanuel4partner@gmail.com>
2025-08-04 13:29:59 +0100

    Week 5 Practice completed

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        Assignment Ass = new Assignment("Great Jackson", "Multiplication");
+        Console.WriteLine(Ass.GetSummary());
+        Console.WriteLine();
+
+        MathAssignment Math = new MathAssignment("Dave Hanson", "Fractions", "7.3", "8-19");
+        Console.WriteLine(Math.GetSummary());
+        Console.WriteLine(Math.GetHomeworkList());
+        Console.WriteLine();
+
+        WritingAssignment writing = new WritingAssignment("Frank Rollers", "European History", "The Causes of World War II");
+        Console.WriteLine(writing.GetSummary());
+        Console.WriteLine(writing.GetWritingInformation());
     }
 }
